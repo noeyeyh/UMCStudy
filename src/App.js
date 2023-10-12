0c15a830b740f9aed7b290fe8159066b8a5fcb4c
NeoSelf1 <neoself1105@gmail.com>
2023-10-12 12:51:15 +0900

    HW3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import HW2 from "./Components/HW2";
-
+import { HW3 } from "./Components/HW3";
+import "./styles.css";
 function App() {
   return (
     <>
-      <HW2 />
+      <HW3 />
     </>
   );
 }
